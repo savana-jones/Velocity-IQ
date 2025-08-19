67f9d7759f1c0999a8519f700c04ebf0ecedf2c4
savana-jones <savana2211@gmail.com>
2025-08-19 14:26:47 +0530

    Remove unnecessary files

--- src/app/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Signup() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold">Sign Up Page (Coming Soon)</h1>
-    </main>
-  );
-}
